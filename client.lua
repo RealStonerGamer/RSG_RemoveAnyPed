e740512dff6da628b4e24e3f04423549042fe554
RSG -Realstonergamer <26008458+RealStonerGamer@users.noreply.github.com>
2024-03-14 22:48:15 -0700

    remove blip to leave just radiusBlip Add: color control for each blip

--- client.lua
+++ client.lua
@@ -1,29 +1,26 @@
-local createdBlips = {} 
+local createdBlips = {}
 
 Citizen.CreateThread(function()
     if Config.debug then
         RegisterCommand("clearBlips", function()
             RemoveAllCreatedBlips()
         end, false)
-        local blipHash = GetHashKey('blip_weapon_throwable') 
         for _, location in pairs(Config.Location) do
-            local blip = Citizen.InvokeNative(0x554D9D53F696D002, 1664425300, vector3(location.coords.x, location.coords.y, location.coords.z))
-            SetBlipSprite(blip, blipHash) 
-            local radiusBlip = Citizen.InvokeNative(0x45f13b7e0a15c880, -1282792512, location.coords.x, location.coords.y, location.coords.z, location.radius)
-            
-            table.insert(createdBlips, {mainBlip = blip, radiusBlip = radiusBlip})
+            local radiusBlip = Citizen.InvokeNative(0x45f13b7e0a15c880, -1282792512, location.coords.x, location.coords
+            .y, location.coords.z, location.radius)
+            Citizen.InvokeNative(0x662D364ABF16DE2F, radiusBlip, GetHashKey(location.blipColor))
+            table.insert(createdBlips, {radiusBlip = radiusBlip })
         end
     end
 end)
 
 function RemoveAllCreatedBlips()
     for _, blipEntry in ipairs(createdBlips) do
-        RemoveBlip(blipEntry.mainBlip)
-        if blipEntry.radiusBlip then 
+        if blipEntry.radiusBlip then
             RemoveBlip(blipEntry.radiusBlip)
         end
     end
-    createdBlips = {} 
+    createdBlips = {}
 end
 
 AddEventHandler('onResourceStop', function(resourceName)
@@ -44,13 +41,14 @@ end
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(1000) 
+        Citizen.Wait(1000)
         local playerCoords = GetEntityCoords(PlayerPedId())
         for _, location in pairs(Config.Location) do
-            local dist = GetDistanceBetweenCoords(playerCoords, location.coords.x, location.coords.y, location.coords.z, true)
+            local dist = GetDistanceBetweenCoords(playerCoords, location.coords.x, location.coords.y, location.coords.z,
+                true)
             if dist < location.radius then
                 CheckAndRemovePeds(location)
-                break 
+                break
             end
         end
     end
@@ -62,13 +60,13 @@ function CheckAndRemovePeds(location)
     for _, modelName in ipairs(location.Pedstoremove) do
         table.insert(pedstoremoveHashes, GetHashKey(modelName))
     end
-
     for i = 1, #allPeds do
         local ped = allPeds[i]
         if IsEntityAPed(ped) and ped ~= PlayerPedId() and not IsPedAPlayer(ped) then
             local pedModelHash = GetEntityModel(ped)
             local pedCoords = GetEntityCoords(ped)
-            local dist = GetDistanceBetweenCoords(pedCoords, location.coords.x, location.coords.y, location.coords.z, true)
+            local dist = GetDistanceBetweenCoords(pedCoords, location.coords.x, location.coords.y, location.coords.z,
+                true)
             if dist < location.radius and table.contains(pedstoremoveHashes, pedModelHash) and not IsPedDeadOrDying(ped, true) then
                 DeleteEntity(ped)
             end
--- config.lua
+++ config.lua
@@ -6,28 +6,34 @@ Config.Location = {
         Pedstoremove = --list of peds to remove from the efected area 
         {"mp_a_c_deer_01","a_c_deer_01","a_c_buck_01","mp_a_c_buck_01","mp_a_c_pronghorn_01","a_c_pronghorn_01","a_c_coyote_01","mp_a_c_coyote_01"},
         coords = vector3(-825.2, -1306.18, 43.6), 
-        radius = 120.0   -- radius around the cords to effect 
+        radius = 120.0,   -- radius around the cords to effect 
+        blipColor = "BLIP_MODIFIER_MP_COLOR_2"--only used when in Debug
     },
     valentine = {
         Pedstoremove = {"mp_a_c_deer_01","a_c_deer_01","a_c_buck_01","mp_a_c_buck_01","mp_a_c_pronghorn_01","a_c_pronghorn_01","a_c_coyote_01","mp_a_c_coyote_01"},
         coords = vector3(-295.86, 790.62, 118.46),
-        radius = 90.0
+        radius = 90.0,
+        blipColor = "BLIP_MODIFIER_MP_COLOR_2"--only used when in Debug
+
     }, 
     StDnise = {
         Pedstoremove = {"mp_a_c_deer_01","a_c_deer_01","a_c_buck_01","mp_a_c_buck_01","mp_a_c_pronghorn_01","a_c_pronghorn_01","a_c_coyote_01","mp_a_c_coyote_01"},
         coords = vector3(2654.09, -1240.02, 53.23),
-        radius = 250.0
-        
+        radius = 250.0,
+        blipColor = "BLIP_MODIFIER_MP_COLOR_2"--only used when in Debug
     },
     Annesburg = {
         Pedstoremove = {"mp_a_c_deer_01","a_c_deer_01","a_c_buck_01","mp_a_c_buck_01","mp_a_c_pronghorn_01","a_c_pronghorn_01","a_c_coyote_01","mp_a_c_coyote_01"},
         coords =vector3(2932.87, 1339.62, 44.05),
-        radius = 90.0
+        radius = 90.0,
+        blipColor = "BLIP_MODIFIER_MP_COLOR_2"--only used when in Debug
     },
     armmadillo= {
         Pedstoremove = {"mp_a_c_deer_01","a_c_deer_01","a_c_buck_01","mp_a_c_buck_01","mp_a_c_pronghorn_01","a_c_pronghorn_01","a_c_coyote_01","mp_a_c_coyote_01"},
         coords =vector3(-3677.07, -2611.77, -14.08),
-        radius = 90.0
+        radius = 90.0,
+        blipColor = "BLIP_MODIFIER_MP_COLOR_2"--only used when in Debug
+
     },
     
 }
